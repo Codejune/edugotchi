fa66c0d69d04a2243a8406fddb456d20efda5c25
codejune <kbj9704@gmail.com>
2019-11-19 15:59:15 +0900

    Theme: Change Bottom navigation bar

---
+++ app/src/main/java/kr/ac/ssu/edugochi/util/ViewUtils.java
@@ -0,0 +1,29 @@
+package kr.ac.ssu.edugochi.util;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.TypedValue;
+
+import androidx.annotation.Nullable;
+
+import kr.ac.ssu.edugochi.R;
+
+public class ViewUtils {
+
+    public static int getThemeAccentColor(final Context context) {
+        final TypedValue value = new TypedValue();
+        context.getTheme().resolveAttribute(R.attr.colorAccent, value, true);
+        return value.data;
+    }
+
+    public static void updateDrawableColor(@Nullable Drawable drawable, int color) {
+        if (drawable == null) return;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            drawable.setTint(color);
+        else
+            drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+    }
+
+}

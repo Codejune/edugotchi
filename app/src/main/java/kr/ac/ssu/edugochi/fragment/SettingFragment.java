0908c865a82302c93f56a25b9913de560e8f80f0
Frics <jimmy9711@naver.com>
2019-11-30 19:37:10 +0900

    developing setting fragment

--- app/src/main/java/kr/ac/ssu/edugochi/fragment/SettingFragment.java
+++ app/src/main/java/kr/ac/ssu/edugochi/fragment/SettingFragment.java
@@ -1,12 +1,16 @@
 package kr.ac.ssu.edugochi.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreferenceCompat;
 
 import es.dmoral.toasty.Toasty;
@@ -16,6 +20,9 @@ import kr.ac.ssu.edugochi.R;
 public class SettingFragment extends PreferenceFragmentCompat {
     private static final String TAG = SettingFragment.class.getSimpleName();
 
+
+
+
     public SettingFragment() {
         // Required empty public constructor
     }
@@ -23,12 +30,24 @@ public class SettingFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences prefs;
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        prefs
+                .registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+                    public void onSharedPreferenceChanged(SharedPreferences sp, String key) {
+                        Log.d("tag","클릭된 Preference의 key는 "+key);
+                    }
+                });
+
 
 
     }
+
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         SwitchPreferenceCompat darkMode = (SwitchPreferenceCompat) findPreference("darkMode");
 
         darkMode.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
@@ -55,16 +74,38 @@ public class SettingFragment extends PreferenceFragmentCompat {
 
         });
 
+        ListPreference WN = (ListPreference)findPreference("WhiteNoise");
+        Log.d(TAG, String.valueOf(WN));
+        Preference reset = findPreference("Reset");
+        reset.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                        .setTitle("초기화 하시겠습니까?")
+                        .setPositiveButton("예", null)
+                         /*
+                         @Override
+                         public void onClick(DialogInterface dialogInterface, int i) {
+                             removeItem(vo);
+                             listItems();
+                         }
+                          */
 
+                        .setNegativeButton("아니오", null)
+                        .create();
+                dialog.show();
+                return false;
+            }
+        });
+        
 
     }
+
+
+
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference, rootKey);
-
-
     }
-
-
 }

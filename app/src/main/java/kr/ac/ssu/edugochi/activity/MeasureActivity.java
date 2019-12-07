04a195f91a05264bb683769ea069df8a83b8cda5
Frics <jimmy9711@naver.com>
2019-12-07 15:48:02 +0900

    turn back measure part

--- app/src/main/java/kr/ac/ssu/edugochi/activity/MeasureActivity.java
+++ app/src/main/java/kr/ac/ssu/edugochi/activity/MeasureActivity.java
@@ -253,8 +253,8 @@ public class MeasureActivity extends AppCompatActivity {
                 SimpleDateFormat today_date = new SimpleDateFormat("yyyy/MM/dd", Locale.KOREA);
                 MeasureData MeasureData = realm.createObject(MeasureData.class);
                 MeasureData.setDate(today_date.format(Calendar.getInstance().getTime()));
-                MeasureData.setTimeout(out_time*1000); // 원래 out_time
-                MeasureData.setExp(out_time); // 원래 out_time/1000 >> 테스트 위해 기준 변경
+                MeasureData.setTimeout(out_time); // 원래 out_time
+                MeasureData.setExp(out_time/1000); // 원래 out_time/1000 >> 테스트 위해 기준 변경
                 MeasureData.setSubject(selected);
                 Log.i(TAG, "date\t\t: " + today_date.format(Calendar.getInstance().getTime()));
                 Log.i(TAG, "timeout\t: " + out_time);
@@ -313,7 +313,7 @@ public class MeasureActivity extends AppCompatActivity {
     private String getTimeOut() {
         long current_time = SystemClock.elapsedRealtime(); //애플리케이션이 실행되고나서 실제로 경과된 시간(??)^^;
         out_time = current_time - base_time;
-        return String.format("%02d : %02d : %02d", out_time / 1 / 60 / 60, (out_time / 1) / 60 % 60, (out_time / 1) % 60 % 60); // 원래 1이아니라 1000 >>테스트 위해 변경
+        return String.format("%02d : %02d : %02d", out_time / 1000 / 60 / 60, (out_time / 1000) / 60 % 60, (out_time / 1000) % 60 % 60); // 원래 1이아니라 1000 >>테스트 위해 변경
     }
 
     @Override
